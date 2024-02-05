3ff6b9cbf8b597410d996193c986477d5f3a477b
Aniket Singh <95512260+ItsAniketSingh@users.noreply.github.com>
2024-02-05 19:46:43 +0530

    Update DeleteNodeInALinkedList.java

--- DeleteNodeInALinkedList.java
+++ DeleteNodeInALinkedList.java
@@ -2,11 +2,8 @@
     237. Delete Node in a Linked List
 
     There is a singly-linked list head and we want to delete a node node in it.
-
     You are given the node to be deleted node. You will not be given access to the first node of head.
-
     All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
-
     Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
 
 The value of the given node should not exist in the linked list.
